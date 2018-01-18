0d533b849019699a34dcc4661e18d37ff2e88a6b
heatherZheng <sz488@cornell.edu>
2018-01-18 15:37:57 +0800

    rcnn_update

--- py-faster-rcnn/tools/videodemo.py
+++ py-faster-rcnn/tools/videodemo.py
@@ -14,8 +14,6 @@ See README.md for installation instructions before running.
 """
 
 import _init_paths
-#import matplotlib
-#matplotlib.use('TkAgg')
 from fast_rcnn.config import cfg
 from fast_rcnn.test import im_detect
 from fast_rcnn.nms_wrapper import nms
@@ -25,6 +23,7 @@ import numpy as np
 import scipy.io as sio
 import caffe, os, sys, cv2
 import argparse
+import time
 
 CLASSES = ('__background__',
            'aeroplane', 'bicycle', 'bird', 'boat',
@@ -35,52 +34,27 @@ CLASSES = ('__background__',
 
 NETS = {'vgg16': ('VGG16',
                   'VGG16_faster_rcnn_final.caffemodel'),
-#		'vgg16_rpn_stage1_iter_60000.caffemodel'),
         'zf': ('ZF',
                   'ZF_faster_rcnn_final.caffemodel')}
 
 
-def vis_detections(im, class_name, dets, thresh=0.5):
+def vis_detections_video(im, class_name, dets, thresh=0.5):
     """Draw detected bounding boxes."""
+
     inds = np.where(dets[:, -1] >= thresh)[0]
     if len(inds) == 0:
-        return
+        return im
 
-    im = im[:, :, (2, 1, 0)]
-    fig, ax = plt.subplots(figsize=(12, 12))
-    ax.imshow(im, aspect='equal')
     for i in inds:
         bbox = dets[i, :4]
         score = dets[i, -1]
+        cv2.rectangle(im,(bbox[0],bbox[1]),(bbox[2],bbox[3]),(0,0,255),2)
+        cv2.rectangle(im,(int(bbox[0]),int(bbox[1])-10),(int(bbox[0]+200),int(bbox[1])+10),(10,10,10),-1)
+        cv2.putText(im,'{:s} {:.3f}'.format(class_name, score),(int(bbox[0]),int(bbox[1]-2)),cv2.FONT_HERSHEY_SIMPLEX,.45,(255,255,255))#,cv2.CV_AA)
+    return im
 
-        ax.add_patch(
-            plt.Rectangle((bbox[0], bbox[1]),
-                          bbox[2] - bbox[0],
-                          bbox[3] - bbox[1], fill=False,
-                          edgecolor='red', linewidth=3.5)
-            )
-        ax.text(bbox[0], bbox[1] - 2,
-                '{:s} {:.3f}'.format(class_name, score),
-                bbox=dict(facecolor='blue', alpha=0.5),
-                fontsize=14, color='white')
-
-    ax.set_title(('{} detections with '
-                  'p({} | box) >= {:.1f}').format(class_name, class_name,
-                                                  thresh),
-                  fontsize=14)
-    plt.axis('off')
-    plt.tight_layout()
-    plt.draw()
-    plt.savefig('/home/zhuyifeng/picFaster.jpg')
-
-def demo(net, image_name):
+def demo_video(net, im):
     """Detect object classes in an image using pre-computed object proposals."""
-
-    # Load the demo image
-    im_file = os.path.join(cfg.DATA_DIR, 'demo', image_name)
-    im = cv2.imread(im_file)
-
-    # Detect all object classes and regress object bounds
     timer = Timer()
     timer.tic()
     scores, boxes = im_detect(net, im)
@@ -89,17 +63,23 @@ def demo(net, image_name):
            '{:d} object proposals').format(timer.total_time, boxes.shape[0])
 
     # Visualize detections for each class
-    CONF_THRESH = 0.8
-    NMS_THRESH = 0.3
+    CONF_THRESH = 0.75
+
+    NMS_THRESH = 0.2
     for cls_ind, cls in enumerate(CLASSES[1:2]):
         cls_ind += 1 # because we skipped background
         cls_boxes = boxes[:, 4*cls_ind:4*(cls_ind + 1)]
+
         cls_scores = scores[:, cls_ind]
         dets = np.hstack((cls_boxes,
                           cls_scores[:, np.newaxis])).astype(np.float32)
         keep = nms(dets, NMS_THRESH)
         dets = dets[keep, :]
-        vis_detections(im, cls, dets, thresh=CONF_THRESH)
+        im=vis_detections_video(im, cls, dets, thresh=CONF_THRESH)
+    cv2.imwrite(os.path.join('output',str(time.time())+'.jpg'),im)
+    cv2.imshow('ret',im)
+    
+    cv2.waitKey(20)
 
 def parse_args():
     """Parse input arguments."""
@@ -120,6 +100,7 @@ if __name__ == '__main__':
     cfg.TEST.HAS_RPN = True  # Use RPN for proposals
 
     args = parse_args()
+    cv2.namedWindow('ret',0)
 
     prototxt = os.path.join(cfg.MODELS_DIR, NETS[args.demo_net][0],
                             'faster_rcnn_alt_opt', 'faster_rcnn_test.pt')
@@ -140,15 +121,23 @@ if __name__ == '__main__':
 
     print '\n\nLoaded network {:s}'.format(caffemodel)
 
-    # Warmup on a dummy image
-    im = 128 * np.ones((300, 500, 3), dtype=np.uint8)
-    for i in xrange(2):
-        _, _= im_detect(net, im)
-
-    im_names = ['004545.jpg']
-    for im_name in im_names:
-        print '~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~'
-        print 'Demo for data/demo/{}'.format(im_name)
-        demo(net, im_name)
-
-    plt.show()
+    ### Load directory of images
+    '''
+    imagePath = '/home/zhuyifeng/py-faster-rcnn/data/demo/004545.jpg'
+    imageFiles = []
+    for f in os.listdir(imagePath):
+        if f.endswith('jpg') or f.endswith('.jpeg'):
+            imageFiles.append(f)
+    imageFiles = sorted(imageFiles)
+    for imageName in imageFiles:
+        image = cv2.imread(os.path.join(imagePath,imageName))
+        demo_video(net,image)
+    '''
+    
+    ### Load Video File
+     
+    videoFilePath = "/home/zhuyifeng/demo1.mp4"
+    videoFile = cv2.VideoCapture(videoFilePath)
+    while True:
+        ret, image = videoFile.read()
+        demo_video(net,image)
